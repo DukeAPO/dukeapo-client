e310b8c14f0cfb850ad8d65c6533b5791924d033
mollymolichen <m.swimstar@gmail.com>
2017-08-28 19:28:17 -0400

    Welcome

---
+++ components/Welcome/Welcome.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import w from './Welcome.css';
+import banner from '../Assets/DukeApoBanner.png';
+
+class Welcome extends React.Component {
+  render(){
+    return (
+      <div className = {w.container} class="container">
+        <h1 className = {w.title} class="title">
+          Welcome to Duke Alpha Phi Omega!
+        </h1>
+        <p className = {}>
+        The Lambda Nu Chapter of Alpha Phi Omega, Duke’s only co-ed service fraternity, was founded in 1955. 
+        We consist of both Pratt and Trinity students alike and contain members of Greek life, Quidditch, 
+        student government, and more. Despite our differences, we’re one big family that serves together, 
+        and we invite you to learn more about our fraternity!
+        </p>
+        <button type="button" className = {w.welcome-button} class="welcome-button" href="eepurl.com/cwEmij">Join our ListServ</button>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
