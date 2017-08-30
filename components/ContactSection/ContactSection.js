29e40ea8e369ba98ed3b018dcde1842766fd07e0
nataliethedoo <natalie_yuuu@yahoo.com.sg>
2017-08-29 22:09:01 -0400

    resized table for contact section

--- components/ContactSection/ContactSection.js
+++ components/ContactSection/ContactSection.js
@@ -7,20 +7,19 @@ class ContactSection extends React.Component {
     return (
       <div>
         <div className={classes.container}>
+          <h1 className={classes.contactHeader}>Contact</h1>
+          <table className={classes.table} border={"1"} style={{width:'100%'}}>
+            <tr className={classes.tr}>
+              <th className={classes.thLeft}><img className={classes.image} src={diaperbank} /></th>
+              <th className={classes.thRight}> 
+              <p className={classes.contactText}>
+                We&#39;re all about working with <br /> our North Carolina community. <br /> To share your event, cause, 
+                or <br /> organization, please fill out this <br /> Google Form  <a href="https://docs.google.com/forms/d/
+                1wKdIaGrDnlfOUKjxTI6Fq8XT_y2Q6oTwmKx9Z9yaUXk/viewform?edit_requested=true" target="_blank">here.</a>                  </p>
+              </th>
+            </tr>
+          </table>
           <div className={classes.smallContainer}>
-            <h1 className={classes.contactHeader}>Contact</h1>
-            <table border={"1"} style={{width:'100%'}}>
-              <tr>
-                <th><img className={classes.image} src={diaperbank} /></th>
-                <th>
-                <p className={classes.contactText}>
-                  We&#39;re all about working with <br /> our North Carolina community. <br /> To share your event, cause, or <br /> organization, please fill out this <br />
-                  Google Form  <a href="https://docs.google.com/forms/d/1wKdIaGrDnlfOUKjxTI6Fq8XT_y2Q6oTwmKx9Z9yaUXk/viewform?edit_requested=true"
-                  target="_blank">here.</a>
-                  </p>
-                </th>
-              </tr>
-            </table>
           </div>
         </div>
       </div>
