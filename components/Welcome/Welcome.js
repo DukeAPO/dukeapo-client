641f9da1c088c1a6c237a3fcc3f9a6f340cbd454
ethanolnoknow <31057180+ethanolnoknow@users.noreply.github.com>
2017-08-30 00:11:11 -0400

    tried centering

--- components/Welcome/Welcome.js
+++ components/Welcome/Welcome.js
@@ -4,15 +4,17 @@ import banner from '../Assets/DukeApoBanner.png';
 
 export const Welcome = () => (
       <div className = {w.container} class="container">
-        <h1 className = {w.title} class="title">
+        <h1 class="title">
           Welcome to Duke Alpha Phi Omega!
         </h1>
-        <p>
-        The Lambda Nu Chapter of Alpha Phi Omega, Duke’s only co-ed service fraternity, was founded in 1955. 
-        We consist of both Pratt and Trinity students alike and contain members of Greek life, Quidditch, 
-        student government, and more. Despite our differences, we’re one big family that serves together, 
-        and we invite you to learn more about our fraternity!
-        </p>
+		<div class="bodyText">
+			<p class="welcome">
+			The Lambda Nu Chapter of Alpha Phi Omega, Duke’s only co-ed service fraternity, was founded in 1955. 
+			We consist of both Pratt and Trinity students alike and contain members of Greek life, Quidditch, 
+			student government, and more. Despite our differences, we’re one big family that serves together, 
+			and we invite you to learn more about our fraternity!
+			</p>
+		</div>
         <button type="button" className = {w.welcomeButton} class="welcome-button" href="eepurl.com/cwEmij">Join our ListServ</button>
       </div>
 );
