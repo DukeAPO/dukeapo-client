aeee62a233705f43289a9ce1c52f34e569e9a81b
Joy Patel <joypatel0123@gmail.com>
2017-08-30 00:53:40 -0700

    Finished welcome.

--- components/Welcome/Welcome.js
+++ components/Welcome/Welcome.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import font from '../CSS/Font.css';
 import w from './Welcome.css';
-import banner from '../Assets/DukeApoBanner.png';
-import FlatButton from 'material-ui/FlatButton';
+import JoinListServeButton from '../JoinListServeButton';
+import SocialButton from '../SocialButton';
+import facebookImage from '../Assets/Facebook.png';
+import instagramImage from '../Assets/Instagram.png';
 
 class Welcome extends React.Component{
 	render(){
-		const listServeButtonStyle = {
-			borderRadius: '16px',
-			border: '1px solid',
-			textAlign: 'center'
-		}
 		const text = {
 		description: 'The Lambda Nu Chapter of Alpha Phi Omega, Duke’s only co-ed service fraternity, was \
 			founded in 1955. We consist of both Pratt and Trinity students alike and contain members of \
 			Greek life, Quidditch, student government, and more. Despite our differences, we’re one big \
 			family that serves together, and we invite you to learn more about our fraternity!',
     };
+		const buttons = [
+			<SocialButton src={facebookImage} href={'https://www.facebook.com/DukeAlphaPhiOmega/'} />,
+			<JoinListServeButton fullWidth={true}/>,
+			<SocialButton src={instagramImage} href={'https://www.instagram.com/dukeapo/'} />
+		];
+		const buttonLi = [];
+		buttons.forEach(function(buttonElement) {
+			buttonLi.push(
+				<li className={w.li}>
+					{buttonElement}
+				</li>
+			);
+		});
+		const buttonOrder = <ul className={w.ul}>{buttonLi}</ul>;
 	return(
-      <div className = {w.container}>
-        <h1 className={w.title}>
-          Welcome to Duke Alpha Phi Omega!
-        </h1>
-		<div class="bodyText">
-			<p className={w.welcome}>
-				{text.description}
-			</p>
-		</div>
-        <FlatButton style={listServeButtonStyle} fullWidth={false} href={'http://eepurl.com/cwEmij'}>
-			<h4 className={'${font.h4} ${w.buttonText}'}>
-				Join our list serv
-			</h4>
-		</FlatButton>
+      <div>
+				<div className = {w.titleContainer}>
+	        <h1 className={`${font.h1} ${w.title}`}>
+	          Welcome to Duke Alpha Phi Omega!
+	        </h1>
+				</div>
+				<div className={`${w.welcomeContainer}`}>
+					<p className={`${font.p} ${w.welcome}`}>
+						{text.description}
+					</p>
+				</div>
+				<div className={`${w.buttonContainer}`}>
+					{buttonOrder}
+				</div>
       </div>
 	);
 	}
