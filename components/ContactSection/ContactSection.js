7012e499ffed15e0bb7a54345efbe7b5758fa96f
nataliethedoo <natalie_yuuu@yahoo.com.sg>
2017-08-29 21:16:44 -0400

    modified contact section

--- components/ContactSection/ContactSection.js
+++ components/ContactSection/ContactSection.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import {
-  Table,
-  TableBody,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
 import classes from './ContactSection.css';
 import diaperbank from '../Assets/apoNCDiaperBank.png';
 
@@ -13,21 +7,18 @@ export const ContactSection = () => (
     <div className={classes.container}>
       <div className={classes.smallContainer}>
         <h1 className={classes.contactHeader}>Contact</h1>
-        <Table>
-          <TableBody displayRowCheckbox={false}>
-            <TableRow displayBorder={false} selectable={false}> 
-              <TableRowColumn className={classes.pictureColumn}><img className={classes.image} src={diaperbank} /></TableRowColumn>
-              <TableRowColumn className={classes.textColumn}>
-              <p className={classes.contactText}>
-              We're all about working with <br /> our North Carolina community. 
-              <br /> To share your event, cause, or <br /> organization, please 
-              fill out this <br /> Google Form  <a href="https://docs.google.com/forms/d/1wKdIaGrDnlfOUKjxTI6Fq8XT_y2Q6oTwmKx9Z9yaUXk/viewform?edit_requested=true" 
+        <table border={"1"} style={"width:100%"}>
+          <tr>
+            <th><img className={classes.image} src={diaperbank} /></th>
+            <th> 
+            <p className={classes.contactText}>
+              We're all about working with <br /> our North Carolina community. <br /> To share your event, cause, or <br /> organization, please fill out this <br /> 
+              Google Form  <a href="https://docs.google.com/forms/d/1wKdIaGrDnlfOUKjxTI6Fq8XT_y2Q6oTwmKx9Z9yaUXk/viewform?edit_requested=true" 
               target="_blank">here.</a> 
               </p>
-              </TableRowColumn>
-            </TableRow>
-          </TableBody>
-        </Table>
+            </th>
+          </tr>
+        </table>
       </div>
     </div>
   </div>
